27222d2b9e585780d185c979918db153f405d036
Fabian Fröhlich <fabian@schaluck.com>
2022-12-26 21:01:21 +0100

    extend datasets

--- training_configuration.py
+++ training_configuration.py
@@ -2,5 +2,10 @@ ALPHAS = {1e-4, 1e-3, 1e-2, 1e-1, 1e-0, 1e1, 1e2}
 LATENT_DIMS = {4, 6, 8}
 CONTEXTS = {'baseline', }
 PATHWAYS = {'EGFR_MAPK', }
-DATASETS = {'synthetic_16_0.5', 'synthetic_16_0.1', 'synthetic_16_0.05', 'synthetic_16_0.01'}
+DATASETS = {
+    'synthetic_16_0.5', 'synthetic_16_0.1', 'synthetic_16_0.05', 'synthetic_16_0.01',
+    'synthetic_32_0.5', 'synthetic_32_0.1', 'synthetic_32_0.05', 'synthetic_32_0.01',
+    'synthetic_64_0.5', 'synthetic_64_0.1', 'synthetic_64_0.05', 'synthetic_64_0.01',
+    'synthetic_128_0.5', 'synthetic_128_0.1', 'synthetic_128_0.05', 'synthetic_128_0.01',
+}
 SPLITS = {'0_5', }
