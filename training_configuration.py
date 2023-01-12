e50f932ec89961b9ec46c7bf753ab86bb9baf4ef
Fabian Fröhlich <fabian@schaluck.com>
2023-01-12 09:42:09 +0100

    switch to real data

--- training_configuration.py
+++ training_configuration.py
@@ -1,11 +1,12 @@
 ALPHAS = {1e-4, 1e-3, 1e-2, 1e-1, 1e-0, 1e1, 1e2}
-LATENT_DIMS = {4, 6, 8}
-CONTEXTS = {'baseline', }
+LATENT_DIMS = {2, 4, 8, 16}
+CONTEXTS = {'baseline', 'init', 'dynamic', }
 PATHWAYS = {'EGFR_MAPK', }
-DATASETS = {
-    'synthetic_16_0.5', 'synthetic_16_0.1', 'synthetic_16_0.05', 'synthetic_16_0.01',
-    'synthetic_32_0.5', 'synthetic_32_0.1', 'synthetic_32_0.05', 'synthetic_32_0.01',
-    'synthetic_64_0.5', 'synthetic_64_0.1', 'synthetic_64_0.05', 'synthetic_64_0.01',
-    'synthetic_128_0.5', 'synthetic_128_0.1', 'synthetic_128_0.05', 'synthetic_128_0.01',
-}
+DATASETS = {'dream_cytof', }
+# DATASETS = {
+#     'synthetic_16_0.5', 'synthetic_16_0.1', 'synthetic_16_0.05', 'synthetic_16_0.01',
+#     'synthetic_32_0.5', 'synthetic_32_0.1', 'synthetic_32_0.05', 'synthetic_32_0.01',
+#     'synthetic_64_0.5', 'synthetic_64_0.1', 'synthetic_64_0.05', 'synthetic_64_0.01',
+#     'synthetic_128_0.5', 'synthetic_128_0.1', 'synthetic_128_0.05', 'synthetic_128_0.01',
+# }
 SPLITS = {'0_5', }
