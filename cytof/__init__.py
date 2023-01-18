a5e60e5916a69bc8ae305ffcf23d503262bba714
FFroehlich <fabian@schaluck.com>
2023-01-18 11:13:57 +0100

    fixup

--- cytof/__init__.py
+++ cytof/__init__.py
@@ -49,7 +49,7 @@ def get_samples(data_name):
             "cUACC893",
             "cZR7530",
         ]
-    elif m := re.match(r'synthetic_([0-9]+)_([0-9\.]+)$', data_name):
+    elif m := re.match(r'synthetic_([0-9]+)_[0-9\.]+_[0-9\.]+$', data_name):
         return [f'sample_{isample}' for isample in range(int(m.group(1)))]
 
     raise ValueError(f'{data_name} is not a valid data name')
