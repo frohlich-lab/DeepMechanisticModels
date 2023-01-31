3e6b1744039938e980633d8e77a7f34ab5c73634
Fabian Fröhlich <fabian@schaluck.com>
2023-01-31 14:14:16 +0100

    fix evaluation

--- evaluate_all.py
+++ evaluate_all.py
@@ -4,8 +4,10 @@ import numpy as np
 import petab
 import seaborn as sns
 import matplotlib.pyplot as plt
+import itertools as itt
 
 from common import fig_dir, EVALUATE_ALL, tpl_evaluation_file, EVALUATION_TRAINING
+from training_configuration import ALPHAS, LATENT_DIMS, CONTEXTS
 
 MODEL = sys.argv[1]
 DATA = sys.argv[2]
@@ -22,6 +24,7 @@ for dataset in ["train", "test"]:
         tpl_evaluation_file.format(samples=SAMPLES, model=MODEL, data=DATA, dataset=dataset, mode='cross_sample'),
         index_col=0
     )
+
     # training
     # training = pd.read_csv(
     #     EVALUATION_TRAINING.format(dataset=dataset, model=MODEL, data=DATA, samples=SAMPLES), index_col=0
@@ -34,13 +37,23 @@ for dataset in ["train", "test"]:
     )
 
     # per sample
-    df_ps = pd.read_csv(
+    ps = pd.read_csv(
         tpl_evaluation_file.format(samples=SAMPLES, model=MODEL, data=DATA, dataset=dataset, mode='per_sample'),
         index_col=0
     )
 
+    avg_ps_dfs = []
+    # copy average/per sample
+    for alpha, ldim, ctxt in itt.product(ALPHAS, LATENT_DIMS, CONTEXTS):
+        for rdf in [avg, ps]:
+            avg_ps_df = rdf.copy()
+            avg_ps_df['alpha'] = alpha
+            avg_ps_df['layers'] = ldim
+            avg_ps_df['context'] = ctxt
+            avg_ps_dfs.append(avg_ps_df)
+
     # dfd = pd.concat([training, pretraining])
-    dfd = pd.concat([pretraining, avg, df_ps])
+    dfd = pd.concat([pretraining, *avg_ps_dfs])
     dfd["dataset"] = dataset
     dfs.append(dfd)
 
@@ -50,23 +63,24 @@ df.loc[df["type"] == "cross_sample", "type"] = "pca embedding"
 df.loc[df["type"] == "full", "type"] = "end-to-end"
 
 for gb in ('observable', 'time', 'condition', 'sample', 'all'):
-    gbs = ("dataset", "type", "latent dim", "l1 regularization")
+    gbs = ["dataset", "type", "latent dim", "l1 regularization"]
     if gb != 'all':
-        gbs = (gb, *gbs)
+        gbs = [gb, *gbs]
     df_gb = pd.DataFrame([
         dict(zip(gbs, group), rmse=np.sqrt(group_df["res"].apply(lambda x: np.power(x, 2)).mean()))
         for group, group_df in df.groupby(gbs)
     ])
 
-    if gb == 'all':
-        data = df_gb
+    if gb == 'time':
+        # filter non-canonical timepoints (not enough datapoints)
+        data = df_gb[np.logical_not(df_gb.time.isin([12, 14, 15, 16, 25, 35]))]
     else:
-        data = df_gb[df_gb.type == 'pca embedding']
+        data = df_gb
 
-    g = sns.FacetGrid(data=data, row="dataset", col="latent dim")
+    g = sns.FacetGrid(data=data, row=gb if gb != 'all' else None, col="latent dim")
     g.map_dataframe(
-        sns.barplot, x=gb if gb != 'all' else "type", y="rmse", hue="l1 regularization"
+        sns.lineplot, x="l1 regularization", y="rmse", style="dataset", hue="type"
     )
-    g.set(yscale="log")
+    g.set(xscale='log', ylim=(0, 1))
     g.add_legend()
     plt.savefig(EVALUATE_ALL.format(model=MODEL, data=DATA, samples=SAMPLES, group=gb))
