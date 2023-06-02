dab35d50c21d829864436503206f70463242881c
Fabian Fröhlich <fabian@schaluck.com>
2023-06-02 20:22:29 +0100

    remove HCC1599

--- cytof/__init__.py
+++ cytof/__init__.py
@@ -20,7 +20,7 @@ def get_samples(data_name):
             "cHCC1419",
             "cHCC1500",
             "cHCC1569",
-            "cHCC1599",
+            # "cHCC1599",
             "cHCC1937",
             "cHCC1954",
             "cHCC2157",
