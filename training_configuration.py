3ae5faa54357e4f01c2914912328df15fd392fd7
Fabian Fröhlich <fabian@schaluck.com>
2023-06-07 16:46:57 +0100

    Update training_configuration.py

--- training_configuration.py
+++ training_configuration.py
@@ -1,10 +1,8 @@
-ALPHAS = {1e-2, 1e-1, 1e-0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6}
-LATENT_DIMS = {2, 4, 6, 8, 10, 12}
-CONTEXTS = {"baseline", "init", "dynamic"}
-PATHWAYS = {"EGFR_MAPK"}
-DATASETS = {
-    "dream_cytof",
-}
+ALPHAS = (1e-2, 1e-1, 1e-0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6)
+LATENT_DIMS = (2, 4, 6, 8, 10, 12)
+CONTEXTS = ("baseline", "init", "dynamic")
+PATHWAYS = ("EGFR_MAPK",)
+DATASETS = ("dream_cytof",)
 # DATASETS = {
 #    'synthetic_16_0.2_0.0',
 #     'synthetic_32_0.2_0.0',
