effa0c2da5eb1b95dfc15787c8db86a6bbb03b39
Giacomo Fabrini <giacomo.fabrini@crick.ac.uk>
2023-11-27 15:51:57 +0000

    Updating training_configuration.py for cluster run
    - trying to follow correlations highlighted in wandb.ai to see which regularisation parameters can drive rmse_val.min down
    - introduced n_hidden = 3 to compare with n_hidden = 2, as 3 can still be visualised

--- training_configuration.py
+++ training_configuration.py
@@ -1,37 +1,60 @@
+# ALPHAS: values for l1reg_inflate: l1 regularisation of layers that inflate from latent/bottleneck to mechanistic parameters
+# from wandb.ai, it seems that rmse_val.min (best value for rmse in validation) is positively correlated with l1reg params
+# i.e. the lower the regularisation, the lower the rmse -- this holds for cytof_init and proteomics, not for transcriptomics
 ALPHAS = (
+    1e1,
+    5e1,
     1e2,
     #1e4,
     #1e6,
     #1e8,
 )
+
+# BETAS: values for oreg_inflate: orthogonal regularisation for layers that inflate from latent/bottleneck to mechanistic parameters
+# from wandb.ai, it seems like oreg params are negatively correlated with rmse_val.min, i.e. the higher the params,
+# the lower the rmse_val.min
 BETAS = (
-    1e2,
+    #1e2,
     #1e4,
-    #1e6
+    1e6,
+    1e7,
     )
 
+# GAMMAS: values for l1reg_encode: l1 regularisation of encoder network (from inputs to latent/bottleneck)
+# same as above - trying to lower this value
 GAMMAS = (
+    1e1,
+    5e1,
     1e2,
     #1e4,
     #1e6,
     #1e8,
 )
 
+# DELTAS: values for oreg_encode: orthogonal regularisation of encoder network (from inputs to latent/bottleneck)
 DELTAS = (
-    1e2,
+    #1e2,
     #1e4,
-    #1e6
+    1e6,
+    1e7,
 )
 
-
+# n_hidden: number of dimensions of bottleneck\latent representation obtained using the encoder
+# from wandb.ai, it does not seem to matter much, but it is slightly positively correlated with rmse_val.min - lower
+# appears to be better? Might be worth trying 3-dim, as it could still be visualised but is in between the values of
+# 2 and 4 that have been tried so far?
+# From wandb, it looks like values above 4 were barely attempted: there is a single run with 6 for proteomics with weird looking results
+# FOR NEXT TIME: try higher values of n_hidden? e.g. 8, 10, 12?
 LATENT_DIMS = (
     2,
-    4,
+    3,
+    #4,
     #6,
     # 8,
     # 10,
     # 12
 )
+
 CONTEXTS_FEATURES = (
     ("cytof_init", "all"),
     # ("cytof_init", "rfe"),
