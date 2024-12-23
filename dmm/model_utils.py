d037a53808fc099442d355855b358573dac5212a
Giacomo Fabrini <giacomo.fabrini@crick.ac.uk>
2024-12-23 10:24:59 +0000

    Make layer size transition smoother in encoder/decoder

--- dmm/model_utils.py
+++ dmm/model_utils.py
@@ -4,6 +4,7 @@ def generate_layer_sizes(
         max_width: int,
         multiplier: int = 2,  # same default value as conf.nn_structure_multiplier
         reverse: bool = False,
+        smoother_transition: bool = True,  # can disable to go back to previous behaviour
 ):
     layer_sizes = []
 
@@ -12,7 +13,12 @@ def generate_layer_sizes(
     size = multiplier * latent_dim
     while len(layer_sizes) < depth and size <= max_width:
         layer_sizes.append(size)
-        size *= multiplier
+        if smoother_transition:
+            # If the hidden layer closest to the input (encoder) / output (inflater) is too narrow compared to the
+            # said input/output, increase the multiplier to bridge the gap (subject to max multiplier value)
+            if (len(layer_sizes) == depth - 1) and (size * (multiplier+1) < max_width):
+                multiplier = min((max_width/multiplier) / size, 5)
+        size = round(size * multiplier)
 
     # If the max width is hit, pad the rest of the layers with encoder_max_width
     while len(layer_sizes) < depth:
