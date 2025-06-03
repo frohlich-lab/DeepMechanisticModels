7eca70f1a24ace313075dedc1d5ad64a334e8726
Fabian Fröhlich <fabian@schaluck.com>
2025-06-03 17:11:48 +0100

    update inflater epoch

--- training_configuration.py
+++ training_configuration.py
@@ -330,7 +330,7 @@ ETAS = {"range": (0,), "central_value": 0}
 # Epoch at which to disable OMEGA regularisation (l1reg_inflater_output)
 # Default: mid-training
 # INFLATER_OUTPUT_REG_EPOCHS = {'range': (50, 100, 200, 300, 500), 'central_value': 100}
-INFLATER_OUTPUT_REG_EPOCHS = {"range": (1000, 200), "central_value": 200}
+INFLATER_OUTPUT_REG_EPOCHS = {"range": (1000, 100), "central_value": 100}
 
 # Percentage thresholds for sparsity
 # SPARSE_THRESH_PERCS = {'range': (5, 10, 25, 50, 75, 100), 'central_value': 50}
