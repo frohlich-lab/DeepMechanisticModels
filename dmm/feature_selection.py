8a84de676cb88146b82914770af40e4dafc38754
Fabian Fröhlich <fabian@schaluck.com>
2025-07-14 17:35:26 +0100

    simplify dropna

--- dmm/feature_selection.py
+++ dmm/feature_selection.py
@@ -328,9 +328,7 @@ def load_data(
         input_data = input_data[features]
     else:
         # for training, compute feature set, filtering out too many nans
-        input_data = input_data.loc[
-            :, input_data.isna().sum() / input_data.shape[0] < 0.5
-        ]
+        input_data = input_data.loc[:, input_data.isna().mean() < 0.5]
         if contextualization == "transcriptomics":
             # look at mean vs variance plot
             # plt.scatter(np.nanmedian(input_data,axis=0),np.nanvar(input_data,axis=0))
