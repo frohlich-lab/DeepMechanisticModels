e4ac53cb4ce777138aba44563964c91d119cc983
Giacomo Fabrini <giacomo.fabrini@crick.ac.uk>
2025-12-04 14:50:22 +0000

    Roll back original directory names for clean repo

--- common.py
+++ common.py
@@ -23,11 +23,11 @@ Wildcards = namedtuple("Wildcards", ["data", "samples"])
 
 basedir: Path = Path(__file__).resolve().parent
 fig_dir = basedir / "figures"
-evaluations_dir = basedir / "eval_p38_noher2"
-results_dir = basedir / "res_p38_noher2"
+evaluations_dir = basedir / "eval"
+results_dir = basedir / "res"
 data_dir = basedir / "data"
-pretrain_dir = basedir / "pretrain_p38"
-features_dir = basedir / "features_p38_noher2"
+pretrain_dir = basedir / "pretrain"
+features_dir = basedir / "features"
 
 PER_SAMPLE_OUTFILE_PARS = str(
     pretrain_dir / "{model}" / "{data}" / "{sample}.csv"
